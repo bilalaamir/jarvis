9c16c3e839b404ffe417d5f134aa9ffaab872448
Faryal Zuberi <fzuberi@creativechaos.co>
2019-11-01 14:31:31 +0500

    add googleDriveSchema on project model

--- models/project.model.js
+++ models/project.model.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const googleDriveSchema = new Schema({
+    id: {type: String},
+    name: {type: String},
+    permissions: { type: Object }
+});
+
 const projectSchema = new Schema({
 
     project_name: {type: String, unique: true},
     status: {type: String, default: 'active'},
     client_slack_channel: {type: Object,},
     project_slack_channel: {type: Object},
-    google_drive_id: {type: String, unique: true},
+    google_drive: {type: googleDriveSchema },
     jira_details: {type: Object},
     start_date: {type: Date},
     end_date: {type: String},
@@ -21,3 +27,4 @@ const projectSchema = new Schema({
 
 const Project = mongoose.model('project', projectSchema);
 module.exports = Project;
+//
