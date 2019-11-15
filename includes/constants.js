f9cf6ecb9a8daa28cddd5424f834671cbbaa26e8
Faryal Zuberi <fzuberi@creativechaos.co>
2019-11-15 18:14:49 +0500

    dynamically determine permission role type in drive service

--- includes/constants.js
+++ includes/constants.js
@@ -1,1 +1,7 @@
-export const 
+module.exports = {
+    ASSOCIATE_ROLES: Object.freeze([
+    { TEAM_MANAGER: 'team_manager'},
+    { PROJECT_OWNER: 'project_owner'},
+    { TEAM_MEMBER: 'team_member'},
+])
+}
